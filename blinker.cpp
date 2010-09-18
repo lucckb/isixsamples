677f2fa2492c28d0e3ad256608876b0cc839fb13
Lucjan Bryndza <lbryndza.p@boff.pl>
2010-09-18 13:13:48 +0200

    Fixed namespace

--- blinker.cpp
+++ blinker.cpp
@@ -25,6 +25,7 @@ namespace
 //Default constructor, construct base object
 ledblink::ledblink():task_base(STACK_SIZE,TASK_PRIO)
 {
+	using namespace stm32;
 	//Enable PE in APB2
 	RCC->APB2ENR |= RCC_APB2Periph_GPIOE;
 	io_config(LED_PORT,LED_PIN,GPIO_MODE_10MHZ,GPIO_CNF_GPIO_PP);
@@ -38,11 +39,11 @@ void ledblink::main()
 	while(true)
 	{
 		//Enable LED
-		io_clr( LED_PORT, LED_PIN );
+		stm32::io_clr( LED_PORT, LED_PIN );
 		//Wait time
 		isix::isix_wait( isix::isix_ms2tick(BLINK_TIME) );
 		//Disable LED
-		io_set( LED_PORT, LED_PIN );
+		stm32::io_set( LED_PORT, LED_PIN );
 		//Wait time
 		isix::isix_wait( isix::isix_ms2tick(BLINK_TIME) );
 	}
--- ledkey.cpp
+++ ledkey.cpp
@@ -27,6 +27,7 @@ namespace
 //Default constructor initialize GPIO and var
 ledkey::ledkey():task_base(STACK_SIZE,TASK_PRIO),is_enabled(false)
 {
+	using namespace stm32;
 	//Enable PE in APB2
 	RCC->APB2ENR |= RCC_APB2Periph_GPIOE;
 	io_config(LED_PORT,LED_PIN,GPIO_MODE_10MHZ,GPIO_CNF_GPIO_PP);
@@ -41,15 +42,15 @@ void ledkey::main()
 	while(true)
 	{
 		//Change state on rising edge
-		if(io_get(KEY_PORT, KEY_PIN) && !p_state)
+		if(stm32::io_get(KEY_PORT, KEY_PIN) && !p_state)
 		{
 			is_enabled = !is_enabled;
 		}
 		//Get previous state
-		p_state = io_get(KEY_PORT, KEY_PIN);
+		p_state = stm32::io_get(KEY_PORT, KEY_PIN);
 		//If enabled change state
-		if(is_enabled) io_clr( LED_PORT, LED_PIN );
-		else io_set( LED_PORT, LED_PIN );
+		if(is_enabled) stm32::io_clr( LED_PORT, LED_PIN );
+		else stm32::io_set( LED_PORT, LED_PIN );
 		//Wait short time
 		isix::isix_wait( isix::isix_ms2tick(DELAY_TIME) );
 	}
