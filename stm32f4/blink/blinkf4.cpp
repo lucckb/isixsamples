2b2b50f16a5f7235a9ad6d8e6269ecbc4bec792c
Lucjan Bryndza <lbryndza.p@boff.pl>
2012-07-20 22:56:55 +0200

    Example update for F1 and F4 platform

--- stm32f4/blink/blinkf4.cpp
+++ stm32f4/blink/blinkf4.cpp
@@ -5,98 +5,28 @@
 #include "config.hpp"
 #include <stm32system.h>
 #include <stm32rcc.h>
+#include <stm32gpio.h>
 /* ------------------------------------------------------------------ */
 namespace {
 /* ------------------------------------------------------------------ */
-//! Configure ADC prescaler to 8
-const unsigned long RCC_CFGR_ADCPRE_8 = (3<<14);
 
-//! HSE oscilator control
-const unsigned long RCC_CR_HSI_ON = (1<<0);
 //Number of isix threads
-
 const unsigned ISIX_NUM_PRIORITIES = 4;
-
 //SysTimer values
 const unsigned MHZ = 1000000;
-const unsigned CTRL_TICKINT_Set = 2;
-const unsigned SysTick_Counter_Enable = 1;
-
 
 /* ------------------------------------------------------------------ */
 /** Cortex stm32 System setup
  * Clock and flash configuration for selected rate
  */
-#if defined(STM32MCU_MAJOR_TYPE_F1)
 void uc_periph_setup()
 {
-	//Configure CLK clock
-    RCC->CR &= ~RCC_CR_HSEON;
-    //Disable Bypass
-    RCC->CR &= ~RCC_CR_HSEBYP;
-    //Enable high speed oscilator
-    RCC->CR  |= RCC_CR_HSEON;
-    //Wait for setup HSE
-    while(1)
-    {
-        if(RCC->CR & RCC_CR_HSERDY) break;
-    }
-    //Configure flash: Prefetch enable and 1 wait state
-    FLASH->ACR = FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY_2;
-
-    /* Configure system clocks ALL clocks freq 16MHz
-     * PLL x 2
-     */
-    RCC->CFGR = RCC_CFGR_ADCPRE_8  | RCC_CFGR_PPRE1_DIV2 | RCC_PLLMul_9 |
-    		RCC_CFGR_PPRE2_DIV1 | RCC_CFGR_PLLSRC;
-    RCC->CFGR2 = RCC_CFGR2_PREDIV1_DIV3;
-
-    // At end disable HSI oscilator for power reduction
-    RCC->CR &= ~RCC_CR_HSI_ON;
-
-    //Enable PLL
-    RCC->CR |= RCC_CR_PLLON;
-
-    //Wait for PLL sync
-    while(1)
-    {
-	    if(RCC->CR & RCC_CR_PLLRDY)
-	    {
-	        //PLL as system clock
-	        RCC->CFGR |=  RCC_CFGR_SW_PLL;
-	        break;
-	    }
-    }
+    stm32::rcc_flash_latency( config::HCLK_HZ );
+    stm32::rcc_pll1_sysclk_setup( stm32::e_sysclk_hse_pll, config::XTAL_HZ , config::HCLK_HZ );
+    stm32::rcc_pclk2_config(  RCC_HCLK_Div2 );
+    stm32::rcc_pclk1_config(  RCC_HCLK_Div4 );
     //Setup NVIC vector at begin of flash
     SCB->VTOR = NVIC_VectTab_FLASH;
-}
-#elif defined(STM32MCU_MAJOR_TYPE_F4)
-
-/* ------------------------------------------------------------------ */
-#else
-#error Selected MCU type is not supported by this sample
-#endif
-/* ------------------------------------------------------------------ */
-//Peripheral setup
-void uc_periph_setup()
-{
-	stm32::rcc_flash_latency( config::HCLK_HZ );
-	stm32::rcc_pll1_sysclk_setup( stm32::e_sysclk_hse_pll, config::XTAL_HZ , config::HCLK_HZ );
-	//Setup dividers
-	RCC->CFGR &= ~(RCC_CFGR_PPRE2 | RCC_CFGR_PPRE1 );
-	RCC->CFGR |= RCC_CFGR_PPRE2_DIV2 | RCC_CFGR_PPRE1_DIV4 | RCC_CFGR_HPRE_DIV1;
-	//Setup NVIC vector at begin of flash
-	SCB->VTOR = NVIC_VectTab_FLASH;
-}
-
-/* ------------------------------------------------------------------ */
-//Setup the systick timer at ISIX_HZ (default 1000HZ)
-void timer_setup()
-{
-	SysTick->LOAD = isix::ISIX_HZ * (config::HCLK_HZ/(8*MHZ));
-	SysTick->CTRL |= CTRL_TICKINT_Set;
-	//System counter enable
-	SysTick->CTRL |= SysTick_Counter_Enable;
 }
 
 /* ------------------------------------------------------------------ */
@@ -126,8 +56,7 @@ void _external_startup(void)
 	//Initialize isix
 	isix::isix_init(ISIX_NUM_PRIORITIES);
 
-	//Setup the systick timer
-	timer_setup();
+	stm32::systick_config( isix::ISIX_HZ * (config::HCLK_HZ/(8*MHZ)) );
 }
 /* ------------------------------------------------------------------ */
 } /* extern C */
@@ -146,10 +75,8 @@ public:
 	ledblink() : task_base(STACK_SIZE,TASK_PRIO), LED_PORT(GPIOE)
 	{
 		using namespace stm32;
-		//Enable PE in APB2
-		RCC->AHB1ENR |= RCC_AHB1Periph_GPIOE;
-		//gpio_config(LED_PORT,LED_PIN,GPIO_MODE_10MHZ,GPIO_CNF_GPIO_PP);
-		gpio_config(LED_PORT, LED_PIN, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_SPEED_25MHZ, GPIO_OTYPE_PP );
+		gpio_clock_enable( LED_PORT, true);
+		gpio_abstract_config(LED_PORT, LED_PIN, AGPIO_MODE_OUTPUT_PP, AGPIO_SPEED_HALF );
 	}
 protected:
 	//Main function
@@ -167,7 +94,7 @@ protected:
 			//Wait time
 			isix::isix_wait( isix::isix_ms2tick(BLINK_TIME) );
 			ala_z *= 1.1;
-			dbprintf("Ala value=%d", (int)ala_z);
+			dbprintf("VAR1=%d", (int)ala_z);
 		}
 	}
 private:
@@ -184,14 +111,8 @@ class ledkey: public isix::task_base
 public:
 	//Constructor
 	ledkey()
-		: task_base(STACK_SIZE,TASK_PRIO), is_enabled(false),
-		  KEY_PORT(GPIOE), LED_PORT(GPIOE)
+		: task_base(STACK_SIZE,TASK_PRIO)
 	{
-		using namespace stm32;
-		//Enable PE in APB2
-		RCC->AHB1ENR |= RCC_AHB1Periph_GPIOE;
-		gpio_config(LED_PORT, LED_PIN, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_SPEED_25MHZ, GPIO_OTYPE_PP );
-		gpio_config(LED_PORT, NOTIFY_PIN, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_SPEED_25MHZ, GPIO_OTYPE_PP );
 	}
 protected:
 	//Main function
@@ -202,26 +123,17 @@ protected:
 		{
 			isix::isix_wait( isix::isix_ms2tick(600) );
 			ala_j += 50.0;
-			dbprintf("Ala2 value=%d", (int)ala_j);
+			dbprintf("VAR2=%d", (int)ala_j);
 		}
 	}
 private:
 		static const unsigned STACK_SIZE = 2048;
 		static const unsigned TASK_PRIO = 3;
-		bool is_enabled;
-		GPIO_TypeDef * const KEY_PORT;
-		GPIO_TypeDef * const LED_PORT;
-		static const unsigned LED_PIN = 15;
-		static const unsigned KEY_PIN = 8;
-		static const unsigned NOTIFY_PIN = 0;
-		static const unsigned KEY_RAISE_LOGIC = 9;
-		static const unsigned KEY_RAISE_INT = 10;
-		static const unsigned DELAY_TIME = 25;
 };
 
 /* ------------------------------------------------------------------ */
 
-}
+}	//namespace app end
 /* ------------------------------------------------------------------ */
 //App main entry point
 int main()
@@ -229,7 +141,6 @@ int main()
 	 dblog_init( stm32::usartsimple_putc, NULL, stm32::usartsimple_init,
 	    		USART2,115200,true, config::PCLK1_HZ, config::PCLK2_HZ );
 	 dbprintf(" Exception presentation app using ISIXRTOS ");
-
 	//The blinker class
 	static app::ledblink led_blinker;
 	//The ledkey class
--- stm32f4/blink/config.hpp
+++ stm32f4/blink/config.hpp
@@ -11,16 +11,25 @@
 /* ------------------------------------------------------------------ */
 namespace config
 {
+#if defined(STM32MCU_MAJOR_TYPE_F1)
+	namespace
+	{
+			//HCLK system speed
+			const unsigned XTAL_HZ =  25000000;
+			const unsigned HCLK_HZ =  75000000;
+			const unsigned PCLK1_HZ = HCLK_HZ/4;
+			const unsigned PCLK2_HZ = HCLK_HZ/2;
+	}
+#elif defined(STM32MCU_MAJOR_TYPE_F4)
 	namespace
 	{
 		//HCLK system speed
-		const unsigned XTAL_HZ = 8000000ul;
-		const unsigned HCLK_HZ = 168000000ul;
+		const unsigned XTAL_HZ = 8000000;
+		const unsigned HCLK_HZ = 168000000;
 		const unsigned PCLK1_HZ = HCLK_HZ/4;
 		const unsigned PCLK2_HZ = HCLK_HZ/2;
 	}
+#endif
 }
-
-
 /* ------------------------------------------------------------------ */
 #endif /* CONFIG_HPP_ */
