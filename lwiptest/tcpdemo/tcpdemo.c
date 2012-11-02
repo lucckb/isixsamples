182210a48d32e463187466bf8838839636447c40
Lucjan Bryndza <lbryndza.p@boff.pl>
2012-11-02 16:28:35 +0100

    TCP network seems to be working

--- lwiptest/tcpdemo/tcpdemo.c
+++ lwiptest/tcpdemo/tcpdemo.c
@@ -21,6 +21,9 @@
 #include <stm32_eth.h>
 #include <stdbool.h>
 #include "tcpecho/tcpecho.h"
+#include "ethernetif.h"
+#include <lwip/tcpip.h>
+#include <lwip/dhcp.h>
 /* ------------------------------------------------------------------ */
 //Led Port
 #define LED_PORT GPIOE
@@ -53,7 +56,67 @@ static ISIX_TASK_FUNC(blinking_task, entry_param)
 
 
 /* ------------------------------------------------------------------ */
-void tcp_eth_init(unsigned hclk);
+
+/**
+  * @brief  Initializes the lwIP stack
+  * @param  None
+  * @retval None
+  *
+  */
+
+static void tcp_eth_init(unsigned hclk)
+{
+  struct ip_addr ipaddr;
+  struct ip_addr netmask;
+  struct ip_addr gw;
+  uint8_t macaddress[6]={0x00,0xA4,0xA5,0x10,0x20,0x30};
+
+  netif_init();
+  tcpip_init( NULL, NULL );
+
+  struct netif *netif =  ethernetif_setup( macaddress, 1, hclk, false, false );
+#if LWIP_DHCP
+  ipaddr.addr = 0;
+  netmask.addr = 0;
+  gw.addr = 0;
+
+#else
+  IP4_ADDR(&ipaddr, 192, 168, 16, 222);
+  IP4_ADDR(&netmask, 255, 255, 255, 0);
+  IP4_ADDR(&gw, 192, 168, 16, 1);
+#endif
+
+
+  /* - netif_add(struct netif *netif, struct ip_addr *ipaddr,
+            struct ip_addr *netmask, struct ip_addr *gw,
+            void *state, err_t (* init)(struct netif *netif),
+            err_t (* input)(struct pbuf *p, struct netif *netif))
+
+   Adds your network interface to the netif_list. Allocate a struct
+  netif and pass a pointer to this structure as the first argument.
+  Give pointers to cleared ip_addr structures when using DHCP,
+  or fill them with sane numbers otherwise. The state pointer may be NULL.
+
+  The init function pointer must point to a initialization function for
+  your ethernet netif interface. The following code illustrates it's use.*/
+  netif_add(netif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &tcpip_input);
+
+  /*  Registers the default network interface.*/
+  netif_set_default(netif);
+
+#if LWIP_DHCP
+  /*  Creates a new DHCP client for this interface on the first call.
+  Note: you must call dhcp_fine_tmr() and dhcp_coarse_tmr() at
+  the predefined regular intervals after starting the client.
+  You can peek in the netif->dhcp struct for the actual DHCP status.*/
+  dhcp_start(netif);
+#endif
+
+  /*  When the netif is fully configured this function must be called.*/
+  netif_set_up(netif);
+
+}
+
 
 
 //Initialize the TCPIP library
