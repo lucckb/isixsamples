4cc97708dcd68c76b0be6ab2caa97fb86944d2cb
Lucjan Bryndza <lbryndza.p@boff.pl>
2012-12-13 22:32:39 +0100

    Rename defs

--- advanced/fatsdio/include/config.h
+++ advanced/fatsdio/include/config.h
@@ -34,6 +34,6 @@
 
 //Enable DMA mode
 //#define CONFIG_ISIX_DRV_SPI_ENABLE_DMAIRQ_MASK ISIX_DRV_SPI_DMAIRQ_SPI1_ENABLE
-#define CONFIG_ISIX_DRV_SPI_ENABLE_DMAIRQ_MASK 1
+#define CONFIG_ISIX_DRV_SPI_ENABLE_DMAIRQ 0
 /* ------------------------------------------------------------------ */
 #endif /* CONFIG_HPP_ */
