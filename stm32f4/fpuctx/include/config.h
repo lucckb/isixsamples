3f08f5173c6152c005cdf282507b10325e4c6ea0
Lucjan Bryndza <lbryndza.p@boff.pl>
2013-12-04 19:17:31 +0100

    Mising config

---
+++ stm32f4/fpuctx/include/config.h
@@ -0,0 +1,15 @@
+/*
+ * config.h
+ *
+ *  Created on: 4 gru 2013
+ *      Author: lucck
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+#define ISIX_CONFIG_SHUTDOWN_API 1
+
+
+#endif /* CONFIG_H_ */
