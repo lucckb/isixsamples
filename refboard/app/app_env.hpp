11f8c350d5578a4691c48df6dd366e6dbf7e9d1b
Lucjan Bryndza <lbryndza.p@boff.pl>
2016-08-29 20:51:22 +0200

    SI5351 initial test

---
+++ refboard/app/app_env.hpp
@@ -0,0 +1,113 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  app_env.hpp
+ *
+ *    Description:  App environment variables
+ *
+ *        Version:  1.0
+ *        Created:  06/24/2014 03:14:25 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Lucjan Bryndza (lb), lucck@boff.pl
+ *   Organization:  BoFF
+ *
+ * =====================================================================================
+ */
+
+#pragma once
+/* ------------------------------------------------------------------ */
+#include <cstddef>
+#include <string>
+#include <type_traits>
+/* ------------------------------------------------------------------ */
+namespace fnd {
+namespace bus {
+	class ibus;
+} }
+/* ------------------------------------------------------------------ */
+namespace app {
+/* ------------------------------------------------------------------ */
+struct envid { // Application config defines
+enum _envid {
+	dummy,
+}; };
+/* ------------------------------------------------------------------ */
+struct env {
+	enum _err {
+		err_not_init = -1
+	};
+};
+/* ------------------------------------------------------------------ */
+/** Init environment variable stuff
+ * @param[in] Internal bus
+ * @return error code
+ */
+void initenv( fnd::bus::ibus& bus );
+
+/* ------------------------------------------------------------------ */
+/** Get environment variable
+ * @param[in] env_id Environment identifier
+ * @param[in] buf Input buffer
+ * @param[in] buf_len Buffer size
+ * @return Error code
+ */
+int setenv( unsigned env_id, const void* buf, size_t buf_len );
+
+template <typename T> inline int setenv( unsigned env_id, const T& value ) {
+	static_assert( std::is_pod<T>::value, "Only POD type is allowed" );
+	return setenv( env_id, &value, sizeof( T ) );
+}
+/* ------------------------------------------------------------------ */
+/** Get environment variable
+ * @param[in] env_id Environment identifier
+ * @param[in] buf input buffer
+ * @param[in] buf_len Buffer size
+ * @return Error code
+ */
+inline int setenv( unsigned env_id, const std::string& str, size_t max_len ) {
+	if( max_len > str.size() ) {
+		max_len = str.size();
+	}
+	return setenv( env_id, str.c_str(), max_len );
+}
+/* ------------------------------------------------------------------ */
+/** Get environment variable
+ * @param[in] env_id Environment identifier
+ * @param[out] buf Output buffer
+ * @param[in] buf_len Buffer size
+ * @return Current number of bytes read or negative error code
+ */
+int getenv( unsigned env_id, void* buf, size_t buf_len );
+
+template <typename T> inline int getenv( unsigned env_id, T& value ) {
+	static_assert( std::is_pod<T>::value, "Only POD type is allowed" );
+	return getenv( env_id, &value, sizeof( T ) );
+}
+/* ------------------------------------------------------------------ */
+/** Get environment and put it into the string
+ * @param[in] env_id Environment identifier
+ * @param[out] buf_str String buffera
+ * @param[in] max_size String max sizea
+ */
+inline int getenv( unsigned env_id, std::string& buf_str, size_t max_len ) {
+	buf_str.resize( max_len );
+	int ret = getenv( env_id, &buf_str[0], max_len );
+	if( ret > 0 ) {
+		buf_str.resize( ret );
+	}
+	return ret;
+}
+/* ------------------------------------------------------------------ */
+/** Unset env
+ * @param[in] env_id Environment id
+ * @return Current status
+ */
+int unsetenv( unsigned env_id );
+/* ------------------------------------------------------------------ */
+/** Delete all envirnment variables
+ *  and reformat filesystem */
+int clearenv( );
+/* ------------------------------------------------------------------ */
+}
--- refboard/app/appmain.cpp
+++ refboard/app/appmain.cpp
@@ -21,7 +21,10 @@
 #include <usart_simple.h>
 #include <stm32gpio.h>
 #include <isix.h>
+#include <isixdrv/i2c_bus.hpp>
 #include "tftdemo.hpp"
+#include "app_env.hpp"
+#include <board/si5351.hpp>
 
 static const auto LED_PORT = GPIOG;
 
@@ -41,10 +44,32 @@ namespace usart_debug {
 #endif
 
 
-void led_blink( void* ) {
+void led_blink( void*  arg ) {
 
 	using namespace stm32;
 	gpio_config_ext( LED_PORT, 0xf0, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_SPEED_2MHZ );
+
+	//Test for environment dupa
+	int res;
+		res = app::setenv( 0, "dupa", 5 );
+		dbprintf( "Result for set %i", res );
+		isix_wait_ms(100);
+	dbprintf( "Setenv results %i\n", res );
+	if( !res ) {
+		for( int i=0; i<8; ++i ) {
+			char buf[8] {};
+			res = app::getenv( 0, buf, sizeof buf );
+			dbprintf( "Setenv results %i %s\n", res, buf );
+		}
+	}
+	//Test ada fruit board
+	auto &si = *reinterpret_cast<Si5351*>( arg );
+	si.init( SI5351_CRYSTAL_LOAD_0PF, 0 );
+	si.set_freq( 1410000000ULL, 0ULL, SI5351_CLK0 );
+	si.set_freq(  710000000ULL, 0ULL, SI5351_CLK1 );
+	si.set_freq(  810000000ULL, 0ULL, SI5351_CLK2 );
+
+	// Test the APP environment
 	 for(int r=0;;r++) {
 		//dbprintf(" SDIO test %i", r );
 		gpio_set_clr_mask( LED_PORT, r<<4, 0xf0 );
@@ -63,7 +88,12 @@ int main() {
 	//The ledkey class
 	static app::tft_tester ft;
 	static dev::keypad kp( ft.get_frame() );
-	isix::task_create( led_blink, nullptr, 512, isix::get_min_priority() );
+	//I2c bus  temporary for tests
+	static stm32::drv::i2c_bus m_i2c { stm32::drv::i2c_bus::busid::i2c1_alt , 400000 };
+	app::initenv( m_i2c );
+	static  Si5351 si5351( m_i2c );
+	//Blink task create
+	isix::task_create( led_blink, &si5351, 2048, isix::get_min_priority() );
 	isix::start_scheduler();
 	return 0;
 }
