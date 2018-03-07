e58ff0a5c8dbd74a6f8918224b18cda95f0b8c8e
Lucjan Bryndza <lbryndza.p@boff.pl>
2018-03-07 22:33:17 +0100

    Usart driver work in progress

--- testf334/app/dts.cpp
+++ testf334/app/dts.cpp
@@ -1,6 +1,11 @@
+#include <cmath>
 #include <periph/dt/dts.hpp>
 #include <periph/gpio/gpio_numbers.hpp>
 #include <periph/dt/dts_config.hpp>
+#include <stm32_ll_usart.h>
+#include <stm32_ll_gpio.h>
+#include <stm32_ll_bus.h>
+#include <stm32_ll_rcc.h>
 
 
 
@@ -10,25 +15,45 @@ namespace _dts_config {
 
 namespace {
 	constexpr clock clk[] {
-		{ bus::ahb1, 72000000 },
-		{ bus::apb1, 30000000 },
-		{ bus::apb2, 60000000 },
+		{	bus::ahb1, []() -> unsigned {
+			 LL_RCC_ClocksTypeDef clk;
+			  LL_RCC_GetSystemClocksFreq(&clk);
+			  return clk.HCLK_Frequency;
+			}
+		},
+		{	bus::apb1, []() -> unsigned {
+			 LL_RCC_ClocksTypeDef clk;
+			  LL_RCC_GetSystemClocksFreq(&clk);
+			  return clk.PCLK1_Frequency;
+			}
+		},
+		{	bus::apb2, []() -> unsigned {
+			 LL_RCC_ClocksTypeDef clk;
+			  LL_RCC_GetSystemClocksFreq(&clk);
+			  return clk.PCLK2_Frequency;
+			}
+		},
+		{	bus::cpu, []() -> unsigned {
+			 LL_RCC_ClocksTypeDef clk;
+			  LL_RCC_GetSystemClocksFreq(&clk);
+			  return clk.SYSCLK_Frequency;
+			}
+		},
 		{}
 	};
 
 	constexpr pin ser0_pins[] {
-		{ pinfunc::sck, gpio::num::PA12 },
-		{ pinfunc::miso, gpio::num::PA13 },
-		{ pinfunc::mosi, gpio::num::PA15 },
-	{}
+		{ pinfunc::txd, gpio::num::PC4 },
+		{}
 	};
 
 	constexpr device devices[]
 	{
 		{
-			"serial0", 0, bus::apb1, 7, 1, ser0_pins
-		}, {
-			"serial1", 0, bus::apb1, 7, 2, ser0_pins
+			"serial0", reinterpret_cast<uintptr_t>(USART1),
+			bus::apb2, LL_GPIO_AF_7,
+			unsigned(std::log2(LL_APB2_GRP1_PERIPH_USART1)),
+			ser0_pins
 		},
 		{}
 	};
