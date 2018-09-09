a03cbb28a559857b6ed03bc7f514ebd059dbfaae
Lucjan Bryndza <lbryndza.p@boff.pl>
2018-09-09 19:24:33 +0200

    Sync

--- testf334/app/test.cpp
+++ testf334/app/test.cpp
@@ -31,10 +31,26 @@
 #include "resources.hpp"
 //SSD1306 display driver
 //https://github.com/kmm/SS1306.git
+// p/x *(SPI_TypeDef*)(0x40000000U+0x00010000U+ 0x00003000U)
 
 
 
 namespace app {
+
+/** Test SPI internal driver based on the new periphlib library
+ */
+void spi_rwtest( periph::drivers::spi_master& spi )  {
+	constexpr uint8_t inbuf[6] { 0x90 };
+	uint8_t outbuf[6] {};
+	static_assert( sizeof outbuf == sizeof inbuf );
+	periph::blk::trx_transfer tran(inbuf,outbuf,sizeof outbuf);
+	dbprintf("Do spirwtest");
+	int err = spi.transaction(1, tran);
+	dbprintf("Transfer error status %i", err );
+	for( auto a: outbuf ) {
+		dbprintf("Return %02x", a);
+	}
+}
 
 //! Test thread for new display library
 void test_thread(void*)
@@ -45,58 +61,49 @@ void test_thread(void*)
 	int ret = m_spi.open(ISIX_TIME_INFINITE);
 	dbprintf("SPI open status %i", ret);
 	ret = m_spi.set_option( opt::speed(10E6) );
-	dbprintf("Set option status 1 %i", ret);
+	dbprintf("Set option speed %i", ret);
 	ret = m_spi.set_option( opt::polarity(opt::polarity::low));
-	dbprintf("Set option status 2 %i", ret);
+	dbprintf("Set option polarity %i", ret);
     ret = m_spi.set_option( opt::phase(opt::phase::_1_edge));
-	dbprintf("Set option status 3 %i", ret);
-/**
-	using smod = drv::spi_device;
-	static constexpr stm32::drv::spi_gpio_config spicnf {
-		CONFIG_PCLK1_HZ,
-		CONFIG_PCLK2_HZ,
-		stm32::GPIO_AF_5,
-		{ stm32::gpio::pin_desc::PB, 4 },	//MISO
-		{ stm32::gpio::pin_desc::PB, 5 },	//MOSI
-		{ stm32::gpio::pin_desc::PB, 3 },	//SCK
-		{{				//CS
-			{ stm32::gpio::pin_desc::PB, 7 },	// MEM_CS - CS0
-			{ stm32::gpio::pin_desc::PB, 6 }	// DI_CS -  CS1
-		}}
-	};
-
-	stm32::drv::spi_master spidev( SPI1, spicnf );
-	spidev.enable( true );
-	static constexpr int bus_speed = 10E6;
-	spidev.set_mode( smod::data_8b|smod::msb_first|smod::polar_cpol_low|smod::phase_1edge, bus_speed/1000, smod::CS1 );
-	stm32::drv::gpio_out rst { GPIOB, 8 };
-	stm32::drv::gpio_out di { GPIOB, 9 };
-	fnd::drv::lcd::ssd1306 disp( spidev, di, rst, smod::CS1, 128, 64 );
-*/
+	dbprintf("Set option phase %i", ret);
+    ret = m_spi.set_option( opt::dwidth(8));
+	dbprintf("Set option data width %i", ret);
+    ret = m_spi.set_option( opt::bitorder(opt::bitorder::msb));
+	dbprintf("Set option bitorder %i", ret);
+#if 1
 	dbprintf("Before SSD");
 	periph::display::ssd1306 disp("display0", m_spi);
 	dbprintf("After SSD");
 	int err = disp.enable(true);
+	for(int a=0;a<5;a++) {
 	disp.set_font( &app::res::font_default);
 	dbprintf("Disp en status %i",err);
+	dbprintf("LOOP>>>%i",a);
 	err = disp.clear();
 	dbprintf("Disp clear status %i",err);
-	err = disp.puts("Ada to nie wypada");
+	err = disp.puts("Ada to nie");
 	dbprintf("Disp puts status %i", err );
 	err = disp.endl();
 	dbprintf("Disp puts status %i", err );
-	err = disp.puts("Display test line2");
+	err = disp.puts("Display line2");
 	dbprintf("Disp puts status %i", err );
 	disp.set_font( &app::res::font_big );
 	err = disp.setpos(0,32);
 	dbprintf("Disp puts setpos %i", err );
 	err = disp.puts("1234");
 	dbprintf("Disp puts status %i", err );
-	//err = disp.progress_bar( 32,16,80,16,50,100);
+	err = disp.progress_bar( 32,16,80,16,50,100);
 	//err = disp.draw_hline( 0, 63, 80, fnd::drv::lcd::color::black);
 	err = disp.show_icon( 12,16,&app::res::manual_icon );
 	dbprintf("draw hline status %i", err );
-	app::hrtim_test_init();
+#endif
+	//app::hrtim_test_init();
+	spi_rwtest(m_spi);
+	spi_rwtest(m_spi);
+	}
+	for(;;) {
+		isix::wait_ms(1000);
+	}
 }
 
 }
