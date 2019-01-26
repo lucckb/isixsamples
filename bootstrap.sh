e1ce1e49ffe71f03490217e78535db9d613b31b1
Lucjan Bryndza <lbryndza.p@boff.pl>
2019-01-26 13:53:15 +0100

    wget script improved

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,15 @@
 #!/bin/sh -e
-wget -O waf https://waf.io/waf-2.0.12
+ver=2.0.12
+hget() {
+	if [ -x $(command -v curl) ]; then
+		curl -o $2 $1
+	elif [ -x $(command -v wget) ]; then
+		wget -O $2 $1
+	else
+		echo "http get program not found"
+	fi
+}
+
+hget https://waf.io/waf-$ver waf
 chmod +x waf
 
