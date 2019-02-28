11a326235abd868577dad8806c579e8c0a8fcb5a
Lucjan Bryndza <lbryndza.p@boff.pl>
2019-02-28 17:14:10 +0100

    Example 4 simple semaphore usage

--- stm32f411e_disco/e3_semaphore/src/semaphore.cpp
+++ stm32f411e_disco/e3_semaphore/src/semaphore.cpp
@@ -1,9 +1,11 @@
-/** Example shows howto use task suspend / reasume features
+/** Example shows howto use semaphores for interasks
  *  Hardware platform: STM32F411E-DISCO
  *  PA2 Port - USART TXD should be connected to serial<->usb converter
  */
 #include <config/conf.h>
 #include <periph/gpio/gpio.hpp>
+#include <foundation/sys/dbglog.h>
+#include <periph/drivers/serial/uart_early.hpp>
 #include <isix.h>
 
 
@@ -14,23 +16,22 @@ namespace {
 }
 
 namespace {
-	// Task for button scanning
-	auto keyb_scan_task( isix::thread& task_ctl ) -> void {
+
+	// Task for button scanning and semaphore signaling
+	auto keyb_scan_task(isix::semaphore& sem,bool& state) -> void {
 		// Configure gpio as input
 		periph::gpio::setup(key_0,
 			periph::gpio::mode::in{periph::gpio::pulltype::down}
 		);
 		// Suspend resume flag
-		bool resume {};
 		for(auto pstate=false;;) {
 			//Get key
 			const auto val = periph::gpio::get(key_0);
 			// On rising edge change state
 			if(val && !pstate) {
-				resume = !resume;
-				//Suspend or resume task
-				if(resume) task_ctl.resume();
-				else task_ctl.suspend();
+				state = !state;
+				// Semaphore signal
+				sem.signal();
 			}
 			//Save previous state
 			pstate = val;
@@ -38,7 +39,7 @@ namespace {
 		}
 	}
 	//Task for the led blinking
-	auto blink_task() -> void {
+	auto blink_task(isix::semaphore& sem,bool& state) -> void {
 		// Configure gpio as output
 		periph::gpio::setup( led_0,
 			periph::gpio::mode::out{
@@ -47,23 +48,27 @@ namespace {
 		} );
 		//! Blinking loop
 		for(;;) {
-			periph::gpio::toggle(led_0);
-			isix::wait_ms(500);
+			// Wait for the semaphore read the variable
+			sem.wait(ISIX_TIME_INFINITE);
+			periph::gpio::set(led_0,state);
 		}
 	}
 }
 
+// Start main function
 auto main() -> int
 {
+	// Create the samaphore and val
+	static isix::semaphore sem { 0, 1 };
+	static bool val {};
 	// Wait some time before startup
     isix::wait_ms( 500 );
 	// Create task for blinking
-	static auto t1 = isix::thread_create_and_run(ISIX_MIN_STACK_SIZE,
-		isix::get_min_priority(), isix_task_flag_suspended, blink_task );
+	static auto t1 = isix::thread_create_and_run(1024,
+		isix::get_min_priority(),0,blink_task,std::ref(sem),std::ref(val));
 	// Create task 2
-	static auto t2 = isix::thread_create_and_run(ISIX_MIN_STACK_SIZE,
-		isix::get_min_priority(),0, keyb_scan_task, std::ref(t1));
+	static auto t2 = isix::thread_create_and_run(1024,
+		isix::get_min_priority(),0,keyb_scan_task,std::ref(sem),std::ref(val));
 	isix::start_scheduler();
 	return 0;
 }
-
