b33724149cda3ef43eef28bd3726a8bfd55329fc
Lucjan Bryndza <lbryndza.p@boff.pl>
2019-08-25 23:28:28 +0200

    missing livedemo

---
+++ stm32f469i_disco/e1_graphics/src/tft_livedemo.hpp
@@ -0,0 +1,21 @@
+/**
+ * =====================================================================================
+ * 	File: tft_livedemo.hpp
+ * 	Created Date: Sunday, August 25th 2019, 8:35:41 pm
+ * 	Author: Lucjan Bryndza
+ * 	Copyright (c) 2019 BoFF
+ * 
+ * 	GPL v2/3
+ * =====================================================================================
+ */
+
+namespace app {
+
+class tft_livedemo
+{
+public:
+    tft_livedemo(tft_livedemo&)=delete;
+    tft_livedemo& operator=(tft_livedemo&)=delete;
+};
+
+}
