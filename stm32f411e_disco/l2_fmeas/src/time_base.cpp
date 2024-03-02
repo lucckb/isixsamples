5d2ae0a634d03a8398c196eab322565dfac94fff
Lucjan Bryndza <lbryndza.p@boff.pl>
2024-03-02 14:54:52 +0100

    Isix lab2

--- stm32f411e_disco/l2_fmeas/src/time_base.cpp
+++ stm32f411e_disco/l2_fmeas/src/time_base.cpp
@@ -4,19 +4,10 @@
 #include <periph/gpio/gpio.hpp>
 #include <isix.h>
 
-namespace {
-    constexpr auto led_0 = periph::gpio::num::PD13;
-}
 
 namespace app {
     void test_thread(void*) {
-        for(int i=0;;++i) {
-            isix::wait_ms(500);
-            if(i%2==0) {
-                dbprintf("Loop %i",i>>1);
-            }
-            periph::gpio::set(led_0, i%2);
-        }
+        for(;;) {}
     }
 }
 
@@ -39,15 +30,8 @@ auto main() -> int
 		periph::drivers::uart_early::open,
 		"serial0", 115200
 	);
-    // Configure PD13 pin LED as an output
-    periph::gpio::setup( led_0,
-        periph::gpio::mode::out{
-            periph::gpio::outtype::pushpull,
-            periph::gpio::speed::low
-        }
-    );
 	isix::task_create( app::test_thread, nullptr, 1536, isix::get_min_priority() );
-    dbprintf("<<<< Hello STM32F411E-DISCO board >>>>");
+    dbprintf("<<<< ISIX sample template >>>>");
 	isix::start_scheduler();
 	return 0;
 }
