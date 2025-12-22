ceee58e8fb67a02bf9ec3ad3f3afd64c81835908
Lucjan Bryndza <lbryndza.p@boff.pl>
2025-12-22 19:09:55 +0100

    ZL41 improved

---
+++ zl41arm/board/dts.cpp
@@ -0,0 +1,13 @@
+#include <periph/dt/dts.hpp>
+#include <periph/gpio/gpio_numbers.hpp>
+#include <periph/dt/dts_config.hpp>
+
+
+
+namespace periph::dt::_dts_config {
+
+// ZL41 ARM empty machine config !!
+constexpr configuration the_machine_config {
+};
+
+}
